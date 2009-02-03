d04d6500dfe82cbbd6e66fce1b927c28893fd33e
Gavin Carr <gavin@openfusion.com.au>
2009-02-04 09:37:13 +1100

    Tweaks to check_by_http verbose output; bump to 0.1.8.

--- lib/Nagios/HTTP/Util.pm
+++ lib/Nagios/HTTP/Util.pm
@@ -17,12 +17,14 @@ sub gen_hash {
   my (%arg) = @_;
   my $cmd = delete $arg{cmd};
   my $freq = delete $arg{freq};
+  my $verbose = delete $arg{verbose};
   croak "Invalid arguments to gen_hash: " . join(',', keys %arg) if keys %arg;
   croak "Missing 'cmd' argument to gen_hash" unless $cmd;
   croak "Missing 'freq' argument to gen_hash" unless $cmd;
 
   my $string = trim($cmd) . "\000" . ($freq || '');
-  return sha256_base64( $string );
+  my $hash = sha256_base64( $string );
+  print STDERR "+ gen_hash: string '$string' -> hash '$hash'\n" if $verbose;
 }
 
 1;
