be862d3decfb990b8ed1f05b803738ab134911f4
Gavin Carr <gavin@openfusion.com.au>
2012-01-16 15:46:34 +1100

    Add --minute/hour/dow support to check_http_result.

--- lib/Nagios/HTTP/Util.pm
+++ lib/Nagios/HTTP/Util.pm
@@ -15,18 +15,19 @@ sub trim {
 
 sub gen_hash {
   my (%arg) = @_;
-  my $cmd = delete $arg{cmd} 
-    or croak "'cmd' is a required parameter\n";
-  my $env = delete $arg{env} || [];
-  my $url = delete $arg{url} || '';
-  my $freq = delete $arg{freq} || '';
+  my $cmd     = delete $arg{cmd} 
+    or croak "Missing 'cmd' argument to gen_hash\n";
+  my $env     = delete $arg{env} || [];
+  my $url     = delete $arg{url} || '';
+  my $freq    = delete $arg{freq} || '';
+  my $minute  = delete $arg{minute} || '';
+  my $hour    = delete $arg{hour} || '';
+  my $dow     = delete $arg{dow} || '';
   my $verbose = delete $arg{verbose};
-  my $logger = delete $arg{logger} || sub { print STDERR "+ $_[0]\n" if $verbose };
+  my $logger  = delete $arg{logger} || sub { print STDERR "+ $_[0]\n" if $verbose };
   croak "Invalid arguments to gen_hash: " . join(',', keys %arg) if keys %arg;
-  croak "Missing 'cmd' argument to gen_hash" unless $cmd;
-  croak "Missing 'freq' argument to gen_hash" unless $cmd;
 
-  my $string = join("\f", trim($cmd), $freq, $url, @$env);
+  my $string = join("\f", trim($cmd), $freq, $minute, $hour, $dow, $url, @$env);
   my $hash = sha256_base64( $string );
   $logger->("gen_hash: string '$string' -> hash '$hash'");
   return $hash;
